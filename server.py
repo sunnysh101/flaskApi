6562ec2fc1d3ce9621357d251af94e0d028798fb
Sunny <shahisunny.990@gmail.com>
2021-07-14 23:17:40 -0700

    Add server.py

---
+++ server.py
@@ -0,0 +1,45 @@
+from sqlite3 import Connection as SQLite3Connection
+from datetime import datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+# app
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+# configure sqlite3 to enforce foreign key constraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+# models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
+    post = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
